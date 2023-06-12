28377a4a61c3324551ce4b850fa7cb057a1441fd
mthcxg <110339956+mthcxg@users.noreply.github.com>
2023-06-12 09:51:29 +0700

    chac chan la final roi

--- display.py
+++ display.py
@@ -44,10 +44,15 @@ with open(path3, "r") as f:
         B.append((c, d))
 
 #mang luu toa do cac duong
-P = [] #1.1 
+
+M = [] #diem giao
+N = []
+P = [] #1.1
 Q = [] #1.2
 C = [] #1.3
-D = [] #2.1
+D = [] #1.4
+R = [] #1.5
+O = [] #1.6
 E = [] #2.2
 F = [] #2.3
 G = [] #2.4
@@ -55,23 +60,29 @@ H = [] #2.5
 I = [] #2.6
 J = [] #2.7
 K = [] #2.8
-M = [] #diem giao
-N = []
-with open("1_chieu1.txt", "r") as file1, open("1_chieu2.txt", "r") as file2, open("1_chieu3.txt", "r") as file3:
+L = [] #2.9
+
+with open("1_chieu1.txt", "r") as file1, open("1_chieu2.txt", "r") as file2, open("1_chieu3.txt", "r") as file3,open("1_chieu4.txt", "r") as file4,open("1_chieu5.txt", "r") as f5,open("1_chieu6.txt", "r") as f6 :
     for line1 in file1:
-        ax, bx = map(int, line1.strip().split('x'))
-        P.append((ax, bx))
+        a, b = map(int, line1.strip().split('x'))
+        P.append((a, b))
     for line2 in file2:
-        cx, dx = map(int, line2.strip().split('x'))
-        Q.append((cx, dx))
+        c, d = map(int, line2.strip().split('x'))
+        Q.append((c, d))
     for line3 in file3:
         e, f = map(int, line3.strip().split('x'))
         C.append((e, f))
-
-with open('2_chieu1.txt', 'r') as file4, open('2_chieu2.txt', 'r') as file5, open('2_chieu3.txt', 'r') as file6, open('2_chieu4.txt', 'r') as file7, open('2_chieu5.txt', 'r') as file8, open('2_chieu6.txt', 'r') as file9, open('2_chieu7.txt', 'r') as file10, open('2_chieu8.txt', 'r') as file11:
     for line4 in file4:
-        a1, b1 = map(int, line4.strip().split('x'))
-        D.append((a1, b1))
+        e1, f1 = map(int, line4.strip().split('x'))
+        D.append((e1, f1))
+    for line5 in f5:
+        e2, f2 = map(int, line5.strip().split('x'))
+        R.append((e2, f2))
+    for line6 in f6:
+        e3, f3 = map(int, line6.strip().split('x'))
+        O.append((e3, f3))
+
+with open('2_chieu2.txt', 'r') as file5, open('2_chieu3.txt', 'r') as file6, open('2_chieu4.txt', 'r') as file7, open('2_chieu5.txt', 'r') as file8, open('2_chieu6.txt', 'r') as file9, open('2_chieu7.txt', 'r') as file10, open('2_chieu8.txt', 'r') as file11,  open('2_chieu9.txt', 'r') as file12:
     for line5 in file5:
         a2, b2 = map(int, line5.strip().split('x'))
         E.append((a2, b2))
@@ -93,6 +104,9 @@ with open('2_chieu1.txt', 'r') as file4, open('2_chieu2.txt', 'r') as file5, ope
     for line11 in file11:
         a8, b8 = map(int, line11.strip().split('x'))
         K.append((a8, b8))
+    for line12 in file12:
+        a9, b9 = map(int, line12.strip().split('x'))
+        L.append((a9, b9))
 
 with open('diem_giao.txt', 'r') as f:
     for l in f:
@@ -162,14 +176,7 @@ distance_to_B = math.sqrt((closest_point_in_B[0]-points[0][0])**2 + (closest_poi
 
 if distance_to_A < distance_to_B:
     x = closest_point_in_A
-    if x in D:
-        common_elements = []
-        for element in D:
-            if element in M:
-                common_elements.append(element)
-        closest_point_x = min(common_elements, key=lambda m: math.sqrt((m[0]-x[0])**2 + (m[1]-x[1])**2))
-        print(closest_point_x)
-    elif x in E:
+    if x in E:
         common_elements = []
         for element in E:
             if element in M:
@@ -218,6 +225,13 @@ if distance_to_A < distance_to_B:
                 common_elements.append(element)
         closest_point_x = min(common_elements, key=lambda m: math.sqrt((m[0]-x[0])**2 + (m[1]-x[1])**2))
         print(closest_point_x)
+    elif x in L:
+        common_elements = []
+        for element in L:
+            if element in M:
+                common_elements.append(element)
+        closest_point_x = min(common_elements, key=lambda m: math.sqrt((m[0]-x[0])**2 + (m[1]-x[1])**2))
+        print(closest_point_x)
 else:
     print("point[0] thuoc duong 1 chieu:", closest_point_in_B)
     x = closest_point_in_B
@@ -245,6 +259,30 @@ else:
                 common_elements.append(element)
         closest_point_x = find_closest_point_by_i(x, common_elements)
         print(closest_point_x)
+    elif x in D:
+        print("x thuoc duong 1_chieu4")
+        common_elements = []
+        for element in D:
+            if element in M:
+                common_elements.append(element)
+        closest_point_x = find_closest_point_by_i(x, common_elements)
+        print(closest_point_x)
+    elif x in R:
+        print("x thuoc duong 1_chieu5")
+        common_elements = []
+        for element in R:
+            if element in M:
+                common_elements.append(element)
+        closest_point_x = find_closest_point_by_i1(x, common_elements)
+        print(closest_point_x)
+    elif x in O:
+        print("x thuoc duong 1_chieu6")
+        common_elements = []
+        for element in O:
+            if element in M:
+                common_elements.append(element)
+        closest_point_x = find_closest_point_by_i1(x, common_elements)
+        print(closest_point_x)
 
 #xet points[1]
 closest_point_in_A1 = min(A, key=lambda z: math.sqrt((z[0]-points[1][0])**2 + (z[1]-points[1][1])**2))
@@ -259,15 +297,7 @@ distance_to_B1 = math.sqrt((closest_point_in_B1[0]-points[1][0])**2 + (closest_p
 if distance_to_A1 < distance_to_B1:
     print("point[1] thuoc duong 2 chieu:", closest_point_in_A1)
     y = closest_point_in_A1
-    if y in D:
-        print("y thuoc duong 2_chieu1")
-        common_elements = []
-        for element in D:
-            if element in M:
-                common_elements.append(element)
-        closest_point_y = min(common_elements, key=lambda m: math.sqrt((m[0]-y[0])**2 + (m[1]-y[1])**2))
-        print(closest_point_y)
-    elif y in E:
+    if y in E:
         print("y thuoc duong 2_chieu2")
         common_elements = []
         for element in E:
@@ -319,6 +349,13 @@ if distance_to_A1 < distance_to_B1:
         print("y thuoc duong 2_chieu8")
         common_elements = []
         for element in K:
+            if element in M:
+                common_elements.append(element)
+        closest_point_y = min(common_elements, key=lambda m: math.sqrt((m[0]-y[0])**2 + (m[1]-y[1])**2))
+        print(closest_point_y)
+    elif y in L:
+        common_elements = []
+        for element in L:
             if element in M:
                 common_elements.append(element)
         closest_point_y = min(common_elements, key=lambda m: math.sqrt((m[0]-y[0])**2 + (m[1]-y[1])**2))
@@ -382,8 +419,60 @@ else:
                     cv2.waitKey(0)
                     cv2.destroyAllWindows()
                     exit()
-
-
+    elif y in D:
+        print("y thuoc duong 1_chieu4")
+        common_elements = []
+        for element in D:
+            if element in M:
+                common_elements.append(element)
+        closest_point_y = find_closest_point_by_j(y, common_elements)
+        print(closest_point_y)
+        if x in D:
+            closest_point_y1 = find_closest_point_by_j1(y, common_elements)
+            print(closest_point_y1)
+            if closest_point_x == closest_point_y1:
+                if points[0][1] < points[1][1]:
+                    cv2.line(img, points[0], points[1], color=(0,0, 255), thickness=2)
+                    cv2.imshow('Image', img)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+                    exit()
+    elif y in R:
+        print("y thuoc duong 1_chieu5")
+        common_elements = []
+        for element in R:
+            if element in M:
+                common_elements.append(element)
+        closest_point_y = find_closest_point_by_i(y, common_elements)
+        print(closest_point_x)
+        if x in R:
+            closest_point_y1 = find_closest_point_by_i1(y, common_elements)
+            print(closest_point_y1)
+            if closest_point_x == closest_point_y1:
+                if points[0][0] < points[1][0]:
+                    cv2.line(img, points[0], points[1], color=(0,0, 255), thickness=2)
+                    cv2.imshow('Image', img)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+                    exit()
+    elif y in O:
+        print("y thuoc duong 1_chieu6")
+        common_elements = []
+        for element in O:
+            if element in M:
+                common_elements.append(element)
+        closest_point_y = find_closest_point_by_j(y, common_elements)
+        print(closest_point_y)
+        if x in O:
+            closest_point_y1 = find_closest_point_by_j1(y, common_elements)
+            print(closest_point_y1)
+            if closest_point_x == closest_point_y1:
+                if points[0][1] > points[1][1]:
+                    cv2.line(img, points[0], points[1], color=(0,0, 255), thickness=2)
+                    cv2.imshow('Image', img)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+                    exit()
 if closest_point_x == (254,131): #diem giao
     x = 'A'
 elif closest_point_x == (344,149):
@@ -392,7 +481,7 @@ elif closest_point_x == (495,179):
     x = 'C'
 elif closest_point_x == (495, 194):
     x = 'D'
-elif closest_point_x == (366,262):
+elif closest_point_x == (367,261):
     x = 'E'
 elif closest_point_x == (496,267):
     x = 'F'
@@ -416,6 +505,12 @@ elif closest_point_x == (462, 577):
     x = 'O'
 elif closest_point_x == (408, 606):
     x = 'P'
+elif closest_point_x == (240, 687):
+    x = 'Q'
+elif closest_point_x == (230, 735):
+    x = 'R'
+elif closest_point_x == (134, 712):
+    x = 'S'
 
 if closest_point_y == (254,131): 
     y = 'A'
@@ -449,7 +544,12 @@ elif closest_point_y == (462, 577):
     y = 'O'
 elif closest_point_y == (408, 606):
     y = 'P'
-
+elif closest_point_y == (240, 687):
+    y = 'Q'
+elif closest_point_y == (230, 735):
+    y = 'R'
+elif closest_point_y == (134, 712):
+    y = 'S'
 
 class Dijkstra:
     def __init__(self, graph, start_vertex):
@@ -532,11 +632,11 @@ class Dijkstra:
         return self.build_path(self.vertex_labels[vertex]['prev']) + [vertex]
 
 
-graph = {'A': {'B': 91.78,},
-         'B': {'A': 91.78, 'C': 153.95, 'E': 115.12},
+graph = {'A': {'B': 91.78,'S': 593.26},
+         'B': {'A': 91.78, 'C': 153.95},
          'C': {'B': 153.95, 'D': 15},
          'D': {'C': 15, 'E': 145.83, 'F': 73.01},
-         'E': {'B': 115.12, 'D': 145.83, 'G': 15.52},
+         'E': {'B': 115.12, 'D': 145.83},
          'F': {'D': 73.01, 'G': 126.4, 'K': 222.93},
          'G' : {'E': 15.52, 'H' : 52.15, 'K': 228.47},
          'H' : {'B': 134.54},
@@ -546,8 +646,12 @@ graph = {'A': {'B': 91.78,},
          'I': {'H': 154.74, 'J': 47.17},
          'J': {'I': 47.17, 'L': 93.62},
          'N': {'K': 81.02, 'O': 34.89},
-         'O': {'N': 34.89, 'K': 90.02, 'P': 61.29},
-         'P': {'L': 90.08, 'O': 61.29}}
+         'O': {'N': 34.89, 'K': 90.02},
+         'P': {'L': 90.08, 'O': 61.29},
+         'Q': {'M': 122.18, 'P': 186.51},
+         'R': {'Q': 49.03, 'S': 98.72},
+         'S': {'R': 98.72}
+         }
 
 dijkstra = Dijkstra(graph, start_vertex= x)
 
@@ -557,9 +661,9 @@ Dijk = dijkstra.build_path(y)
 print(x, '->' ,y,':', Dijk)
 
 
-Arr = [(254,131), (344,149),(495,179), (495, 194), (366,262), (496,267), (370,277),(318,281), (289,433),
-       (336, 437), (460, 487),(373, 523),(263, 567), (493, 561),(462, 577), (408, 606) ]
-arr = ['A', 'B', 'C', 'D', 'E', 'F','G','H', 'I', 'J','K', 'L', 'M', 'N','O', 'P']
+Arr = [(254,131), (344,149),(495,179), (495, 194), (367,261), (496,267), (370,277),(318,281), (289,433),
+       (336, 437), (460, 487),(373, 523),(263, 567), (493, 561),(462, 577), (408, 606), (240,687), (230,735), (134,712) ]
+arr = ['A', 'B', 'C', 'D', 'E', 'F','G','H', 'I', 'J','K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S']
 dict_mapping = {}
 for i in range(len(Arr)):
     dict_mapping[arr[i]] = Arr[i]
@@ -572,16 +676,19 @@ for i in Dijk:
     if i in dict_mapping:
         result.append(dict_mapping[i])
 
-arr_list = [D,E,F,G,H,I,J,K]
-Arr_list = [P,Q,C]
+arr_list = [E,F,G,H,I,J,K, L]
+Arr_list = [P,Q,C, D, R, O]
 #hien thi len anh
 if distance_to_A < distance_to_B: #diem 1 nam tren duong 2 chieu
     result.insert(0, points[0])
     result.insert(1,closest_point_in_A)
-    print(result[2][0])
+    print(result)
     if result[2][0] <= points[0][0] <= result[3][0] and result[2][1] <= points[0][1] <= result[3][1]:
         del result[2]
         print('a')
+    elif result[2][0] >= points[0][0] >= result[3][0] and result[2][1] <= points[0][1] <= result[3][1]:
+        del result[2]
+        print('m')
     if distance_to_A1 < distance_to_B1 : #diem 2 nam tren duong 2 chieu
         for arr in arr_list:
             if closest_point_in_A1 in arr and closest_point_in_A in arr: #tren cung 1 duong
@@ -621,6 +728,7 @@ if distance_to_A < distance_to_B: #diem 1 nam tren duong 2 chieu
 
     else: #diem 2 nam tren duong 1 chieu
             result.append(closest_point_in_B1)
+            print('c')
             result.append(points[1])
             p = np.array(result)
             print(result)
